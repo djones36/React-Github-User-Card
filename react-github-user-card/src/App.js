e9c2819f982ce0cc9049057e12b686b007f5dd27
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-03 12:37:21 -0700

    created app adn dependcies

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Github User Card</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
