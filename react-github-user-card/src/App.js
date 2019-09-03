04bd204523da5733b24a42f41906d76d71647577
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-03 14:17:06 -0700

    data showing in console

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { axoisUser, axiosFollowers } from "./components/Axios";
+import axios from "axios";
+// import { axoisUser, axiosFollowers } from "./components/Axios";
 import Cards from "./components/Card";
 class App extends React.Component {
   constructor() {
@@ -11,17 +12,43 @@ class App extends React.Component {
     };
   }
   componentDidMount() {
-    this.axoisUser();
-    this.axiosFollowers();
+    this.dataUser();
+    this.dataFollowers();
   }
+  dataUser = () => {
+    axios
+      .get("https://api.github.com/users/djones36")
+      .then(res => {
+        console.log("user ", res.data);
+        this.setState({ user: res.data });
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  };
+
+  dataFollowers = () => {
+    axios
+      .get("https://api.github.com/users/djones36/followers")
+      .then(res => {
+        console.log("followers ", res.data);
+        this.setState({ followers: res.data });
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  };
+
   render(user, followers) {
     return (
       <div className="App">
         <h1>Github User Card</h1>
         <div className="user">
+          <h2>User's Profile</h2>
           <Cards value={this.state.user} />
         </div>
         <div className="followers">
+          <h2>Follower's Profiles</h2>
           <Cards value={this.state.followers} />
         </div>
       </div>
